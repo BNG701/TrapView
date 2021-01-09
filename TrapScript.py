a8ff75ce0604a842ac259b680a5ce23c62b71a65
BNG701 <48618359+BNG701@users.noreply.github.com>
2021-01-09 17:25:05 +0100

    Add files via upload

--- TrapScript.py
+++ TrapScript.py
@@ -1,55 +1,58 @@
 import os
-os.system('python -m pip install Figlet')
-os.system('python -m pip install pyfiglet')
+#os.system('python -m pip install Figlet')
+#os.system('python -m pip install pyfiglet')
 import webbrowser
 import pyfiglet
 from pyfiglet import Figlet
 import socket
 import datetime
-
 import sys
 
-
-
-os.system("clear")
 ascii_banner = pyfiglet.figlet_format("TrapView")
-print('                                Version 1.1')
-print(ascii_banner)
-print("___________________________________________")
-print("                           By @Dr_sans_fil ")
-print("___________________________________________")
-print("-------------------------------------------")
+print('\u001b[32m' +ascii_banner)
+print(" ___________________________________________")
+print("                          \u001b[31m By @Dr_sans_fil ")
+print("\u001b[32m ___________________________________________")
+print("\u001b[0m -------------------------------------------")
 print("Outil De scan des ports via une adresse ip \n EX : xxx.xxx.xxx.xxx")
 print("-------------------------------------------")
 
-ipadress=input('[+] Entrez une adresse ip :')
-print('Entrez une plage de port a Scanner : ')
-debut=input('Allant de  : ')
-fin=input('a  : ')
-print('_________ VERIFICATION _____________')
-print('Ip: '+ ipadress + ' Plage de port: ('+debut+':'+fin+')' )
-print('_______ LANCEMENT DU SCAN ___________')
-date = datetime.datetime.now()
-rapport = open("Rapport ["+ipadress+"].txt", "a")
-rapport.write('========================= DEBUT ===================================\n')
-rapport.write('['+str(date)+'] Debut du scan de ['+str(ipadress)+'] port ('+str(debut)+':'+str(fin)+')\n')
 def scan_port(ipadress,port):
 	try:
 		sock = socket.socket()
 		sock.connect((ipadress,port))
-		print('[+] port ' + str(port)+ ' is open')
-		rapport.write('[+] port ' + str(port)+ ' is open\n')
+		print('\u001b[32m [+] \u001b[0m : port ' + str(port) + ' is open')
+		rapport.write('[+] port ' + str(port) + ' is open\n')
+
 	except:
-		print('[-] port ' + str(port)+ ' is closed')
-		rapport.write('[-] port ' + str(port)+ ' is closed\n')
-for port in range(int(debut),int(fin)):
-	scan_port(ipadress, port)
-print('_______ FIN DU SCAN ___________')
-rapport.write('========================= FIN ===================================\n')
-print('[+] Creation du rapport du scan .... \n File: Rapport['+ipadress+'].txt')
+		print('\u001b[31m [-] \u001b[0m : port ' + str(port) + ' is closed')
+		rapport.write('[-] port ' + str(port) + ' is closed\n')
 
-rapport.close()
-print('[+] Creation du rapport Terminer ')
-webbrowser.open("Rapport["+ipadress+"].txt")
 
-	
+ipadress=input('[+] Entrez une adresse Ip/Url :')
+print('Entrez une plage de port a Scanner ')
+debut :int =input('Allant de  : ')
+fin :int=input('a  : ')
+s =int(fin)-int(debut)
+
+if int(s)<3:
+	print("\u001b[31m [Error] \u001b[0m : Veuillez Selectionner une plage d'aumoins 3 ports !")
+else:
+	date = datetime.datetime.now()
+	rapport = open("Rapport ["+ipadress+"].txt", "a")
+	rapport.write('\n['+str(date)+'] Debut du scan de ['+str(ipadress)+'] port ('+str(debut)+':'+str(fin)+')\n')
+	print('_______ DEBUT DU SCAN ___________')
+	for port in range(int(debut),int(fin)):
+		scan_port(ipadress, port)
+
+	print('_______ FIN DU SCAN ___________')
+	print("\u001b[32m Possibilites d\'ataque :" + str(port_open) )
+	rapport.write('========================= FIN ===================================\n')
+	print('\u001b[0m [+] Creation du rapport du scan .... \n File: Rapport['+ipadress+'].txt')
+	rapport.close()
+	print('[+] Creation du rapport Terminer ')
+
+	#print("\u001b[31m [Error] \u001b[0m : Veuillez Selectionner une option valide !")
+
+
+
