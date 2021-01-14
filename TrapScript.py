dc91ae5701a3a7575f8cb42703dbad18a05b057b
BNG701 <48618359+BNG701@users.noreply.github.com>
2021-01-14 11:37:14 +0100

    Update TrapScript.py

--- TrapScript.py
+++ TrapScript.py
@@ -2,28 +2,44 @@ import os
 #os.system('python -m pip install Figlet')
 #os.system('python -m pip install pyfiglet')
 import webbrowser
-import pyfiglet
-from pyfiglet import Figlet
 import socket
 import datetime
 import sys
-os.system('clear')
-ascii_banner = pyfiglet.figlet_format("TrapView")
-print('\u001b[32m' +ascii_banner)
-print(" ___________________________________________")
-print("                          \u001b[31m By @Dr_sans_fil ")
-print("\u001b[32m ___________________________________________")
-print("\u001b[0m -------------------------------------------")
+print("\u001b[31m###########################################################")
+print("\u001b[31m##################################################\u001b[32m   \u001b[31m######")
+print("\u001b[31m##############################################\u001b[32m   /~\   \u001b[31m####")
+print("\u001b[31m###########################################\u001b[32m   _- `~~~', \u001b[31m###")
+print("\u001b[31m#########################################\u001b[32m  _-~       )  \u001b[31m###")
+print("\u001b[31m######################################\u001b[32m  _-~          |  \u001b[31m###")
+print("\u001b[31m###################################\u001b[32m  _-~            ;  \u001b[31m####")
+print("\u001b[31m#########################\u001b[32m  __---___-~              |   \u001b[31m####")
+print("\u001b[31m######################\u001b[32m   _~   ,,                  ;  `,,  \u001b[31m#")
+print("\u001b[31m####################\u001b[32m  _-~    ;'                  |  ,'  ; \u001b[31m#")
+print("\u001b[31m##################\u001b[32m  _~      '                    `~'   ; \u001b[31m##")
+print("\u001b[31m###########\u001b[32m   __---;                                 ,' \u001b[31m###")
+print("\u001b[31m#######\u001b[32m   __~~  ___     TrapView v1.1              ,' \u001b[31m#####")
+print("\u001b[31m####\u001b[32m  _-~~   -~~ _                               ,' \u001b[31m#######")
+print("\u001b[31m####\u001b[32m `-_         _                              ; \u001b[31m#########")
+print("\u001b[31m######\u001b[32m  ~~----~~~   ;                          ; \u001b[31m##########")
+print("\u001b[31m########\u001b[32m  /          ;                        ; \u001b[31m###########")
+print("\u001b[31m######\u001b[32m  /             ;                      ; \u001b[31m############")
+print("\u001b[31m####\u001b[32m  /                `                    ; \u001b[31m#############")
+print("\u001b[31m##\u001b[32m  /                                      ; \u001b[31m##############")
+print("\u001b[31m#\u001b[32m /                                       ;\u001b[31m################")
+print("\u001b[33m##################################### By @Dr_sans_fil #####\u001b[0m")
 print("Outil De scan des ports via une adresse ip \n EX : xxx.xxx.xxx.xxx")
 print("-------------------------------------------")
-
+ok=0
 def scan_port(ipadress,port):
+	global ok
 	try:
 		sock = socket.socket()
 		sock.settimeout(1)
 		sock.connect((ipadress,port))
-		print('\u001b[32m [+] \u001b[0m : port ' + str(port) + ' is open')
-		rapport.write('[+] port ' + str(port) + ' is open\n')
+		print('\u001b[32m [ OK ] \u001b[0m : port ' + str(port) + ' is open')
+		rapport.write('[ OK ] port ' + str(port) + ' is open\n')
+		file_port.write(" "+str(port)+",")
+		ok=ok+1
 
 	except:
 		print('\u001b[31m [-] \u001b[0m : port ' + str(port) + ' is closed')
@@ -43,8 +59,10 @@ try:
 	else:
 		date = datetime.datetime.now()
 		rapport = open("Rapport ["+ipadress+"].txt", "a")
+		file_port = open("ports.txt", "a")
 		rapport.write('\n['+str(date)+'] Debut du scan de ['+str(ipadress)+'] port ('+str(debut)+':'+str(fin)+')\n')
 		print('__________ DEBUT DU SCAN ______________')
+
 		for port in range(int(debut),int(fin)):
 			scan_port(ipadress, port)
 
@@ -53,18 +71,21 @@ try:
 		print('\u001b[0m [+] Creation du rapport du scan .... \n File: Rapport['+ipadress+'].txt')
 
 		print('[+] Creation du rapport Terminer ')
+		print("Nombre de port ouvert : " + str(ok))
+
 		plus =input(" \u001b[33m Recuperer plus d'info ? \u001b[37m [y/n]:")
+
 		if plus == "y":
 			plus_text = os.system('whois '+ ipadress+'')
+			rapport.write(str(plus_text))
+		else:
+			print("\u001b[32m --__--__--__-- FIN --__--__--__--__--__")
 
-
-		print("\u001b[32m --__--__--__-- FIN --__--__--__--__--__")
-		rapport.write(str(plus_text))
 
 except socket.error:
 	print("\u001b[31m [Fatal Error] \u001b[0m : Ip Invalide Ex :[xxx.xxx.xxx.xxx]")
 rapport.close()
-	#print("\u001b[31m [Error] \u001b[0m : Veuillez Selectionner une option valide !")
+
 
 
 
